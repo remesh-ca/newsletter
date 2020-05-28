14b2a4507ec7479a14b83aa8875b82851ac7c75d
Remesh <remeshcanandan14@gmail.com>
2020-05-28 15:10:24 +0530

    set startpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started working on port 3000!");
 });
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
